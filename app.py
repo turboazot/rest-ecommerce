3e296e92db92c53f8c9327c5979ecea8b19d17c5
turboazot <zntu1995@gmail.com>
2021-08-20 03:16:33 +0300

    Implement get products endpoint

--- app.py
+++ app.py
@@ -54,6 +54,13 @@ def add_product():
 
   return product_schema.jsonify(new_product)
 
+# Get All Products
+@app.route('/product', methods=['GET'])
+def get_products():
+  all_products = Product.query.all()
+  result = products_schema.dump(all_products)
+  return jsonify(result)
+
 
 # Run Server
 if __name__ == '__main__':
