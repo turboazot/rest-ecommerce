f2ef2dec9e25f5c42692c288aa68ca622b383397
turboazot <zntu1995@gmail.com>
2021-10-07 04:19:03 +0300

    Fix logging

--- api/app.py
+++ api/app.py
@@ -11,26 +11,6 @@ def create_app(config=api.config.Config):
     app = flask.Flask(__name__)
     app.config.from_object(config)
 
-    dictConfig({
-        'version': 1,
-        'formatters': {
-            'default': {
-                'format':
-                '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-            }
-        },
-        'handlers': {
-            'wsgi': {
-                'class': 'logging.StreamHandler',
-                'stream': 'ext://flask.logging.wsgi_errors_stream',
-                'formatter': 'default'
-            }
-        },
-        'root': {
-            'level': app.config['LOG_LEVEL']
-        }
-    })
-
     app.url_map.strict_slashes = False
 
     register_blueprints(app)
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p /tmp/prometheus
-PROMETHEUS_MULTIPROC_DIR=/tmp/prometheus gunicorn --timeout=120 -b 0.0.0.0:5000 --log-file=gunicorn.log manage:app --access-logfile - --workers=3
+PROMETHEUS_MULTIPROC_DIR=/tmp/prometheus gunicorn --timeout=120 -b 0.0.0.0:5000 --log-level DEBUG --log-file=gunicorn.log manage:app --access-logfile=access.log --error-logfile=error.log --workers=3
